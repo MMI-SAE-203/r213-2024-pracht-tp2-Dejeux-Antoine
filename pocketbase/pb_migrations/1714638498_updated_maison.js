3c68dd534b456d65681a4e2ec02d2f0619b4eb0a
Dejeux-Antoine <150045949+Dejeux-Antoine@users.noreply.github.com>
2024-05-02 10:36:29 +0200

    ajt de ma bdd

---
+++ pocketbase/pb_migrations/1714638498_updated_maison.js
@@ -0,0 +1,48 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("0vwm8kqkc6n47nn")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "zvlytht3",
+    "name": "image",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [],
+      "thumbs": [],
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("0vwm8kqkc6n47nn")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "zvlytht3",
+    "name": "images",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [],
+      "thumbs": [],
+      "maxSelect": 99,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,57 +1,99 @@
 <script setup lang="ts">
-//
 const maisonsListe: {
-  adresse: string;
-  collectionId: string;
+    adresse: string;
+    collectionId: string;
+    collectionName: string;
+    created: string;
+    favori: boolean;
+    id: string;
+    image: string;
+    nbChambres: number;
+    nbSdb: number;
+    nomMaison: string;
+    prix: number;
+    surface: number;
+    updated: string;
+}[] = [
+  {
+    "adresse": "3 rue du bleu",
+    "collectionId": "0vwm8kqkc6n47nn",
+    "collectionName": "maison",
+    "created": "2024-04-12 12:08:43.233Z",
+    "favori": false,
+    "id": "i1sf7kl3spd9gjn",
+    "image": "224001738_a_26brO2nUtx.jpg",
+    "nbChambres": 3,
+    "nbSdb": 1,
+    "nomMaison": "MaisonBleu",
+    "prix": 155000,
+    "surface": 100,
+    "updated": "2024-04-12 12:43:03.520Z"
+  },
+  {
+    "adresse": "Château de Chambord, 41250 Chambord",
+    "collectionId": "0vwm8kqkc6n47nn",
+    "collectionName": "maison",
+    "created": "2024-04-12 12:30:39.355Z",
+    "favori": true,
+    "id": "69muwuji1mbnixc",
+    "image": "chambord_septembre_2023_dnc_olivier_marchant_1_1600x900_N7xx93Ple9.jpg",
+    "nbChambres": 54,
+    "nbSdb": 24,
+    "nomMaison": "chateau",
+    "prix": 54480000,
+    "surface": 4520,
+    "updated": "2024-04-12 12:31:21.737Z"
+  },
+  {
+    "adresse": "1026 rue de York à Elmore",
+    "collectionId": "0vwm8kqkc6n47nn",
+    "collectionName": "maison",
+    "created": "2024-04-12 12:39:41.641Z",
+    "favori": true,
+    "id": "ypv2dybo21wu1d9",
+    "image": "mgxg9h4zlbm51_e5cTRj40t6.webp",
+    "nbChambres": 3,
+    "nbSdb": 1,
+    "nomMaison": "Maison des Watterson",
+    "prix": 25000,
+    "surface": 100,
+    "updated": "2024-04-12 12:39:41.641Z"
+  },
+  {
+    "adresse": "5 rue de la merde",
+    "collectionId": "0vwm8kqkc6n47nn",
+    "collectionName": "maison",
+    "created": "2024-04-12 12:42:43.570Z",
+    "favori": false,
+    "id": "wy7wsnedsdjpq84",
+    "image": "sans_titre_n5uXYDoTjI.jpg",
+    "nbChambres": 1,
+    "nbSdb": 0,
+    "nomMaison": "MaisonTerre",
+    "prix": 1000,
+    "surface": 15,
+    "updated": "2024-04-12 12:42:43.570Z"
+  },
+  {
+    "adresse": "5 rue de herobraine",
+    "collectionId": "0vwm8kqkc6n47nn",
+    "collectionName": "maison",
+    "created": "2024-04-12 12:47:09.883Z",
+    "favori": true,
+    "id": "abzo8pwz4hnk86o",
+    "image": "maxresdefault_zPjdWx9EwR.jpg",
+    "nbChambres": 8,
+    "nbSdb": 3,
+    "nomMaison": "VillaDeLuxe",
+    "prix": 1000000,
+    "surface": 369,
+    "updated": "2024-04-12 12:47:09.883Z"
+  }
+]
+
 </script>
 
 <template>
   <h1 class="text-2xl">Bonjour monde !</h1>
-  const maisonsListe: MaisonRecord[] =  [
-    {
-        "adresse": "2821 Lake Sevilla, Palm Harbor, TX",
-        "collectionId": "sidpkt9hmbfwoeb",
-        "collectionName": "maison",
-        "created": "2023-05-01 14:00:54.598Z",
-        "favori": false,
-        "id": "64l3n8nar836i5j",
-        "image": "pexels_binyamin_mellish_106399_bnHdy6H03e.jpg",
-        "nbChambres": 4,
-        "nbSdb": 2,
-        "nomMaison": "Beverly Springfield",
-        "prix": 2700,
-        "surface": 45,
-        "updated": "2024-04-02 13:49:11.230Z"
-    },
-    {
-        "adresse": "2699 Green Valley, Highland Lake",
-        "collectionId": "sidpkt9hmbfwoeb",
-        "collectionName": "maison",
-        "created": "2024-04-02 13:48:47.643Z",
-        "favori": true,
-        "id": "lo831zeg10hpm9y",
-        "image": "pexels_binyamin_mellish_106399_YpoLMcQJsi.png",
-        "nbChambres": 4,
-        "nbSdb": 2,
-        "nomMaison": "Palm Harbor",
-        "prix": 2095,
-        "surface": 42,
-        "updated": "2024-04-02 13:48:47.643Z"
-    },
-    {
-        "adresse": "909 Woodland St, Michigan, IN",
-        "collectionId": "sidpkt9hmbfwoeb",
-        "collectionName": "maison",
-        "created": "2024-04-02 13:52:17.823Z",
-        "favori": false,
-        "id": "s1zuhj19x8eb242",
-        "image": "faulkner_ave_ZHz3nhmJzC.png",
-        "nbChambres": 4,
-        "nbSdb": 3,
-        "nomMaison": "Faulkner Ave",
-        "prix": 4550,
-        "surface": 80,
-        "updated": "2024-04-02 13:52:17.823Z"
-    }
-];
+
 </template>
